b5309cb7e35aa14fa7211b1c625981c2862e8a13
Pierre G <pierre@pastacode.fr>
2022-07-11 15:01:08 +0200

    Add local feature importance chart

--- src/dashboard_functions.py
+++ src/dashboard_functions.py
@@ -1,14 +1,20 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import pickle
 import requests
 import seaborn as sns
 import streamlit as st
 
+from lime import lime_tabular
+
 # Loading the dataset used for training the data
 TRAIN_SET_PATH = './data/train_set.csv'
 TEST_SET_PATH = './data/test_set.csv'
+MODEL_PATH = './model/lgbm.pkl'
 PREDICTIONS_LABELS = ['CREDIT ACCORDE', 'CREDIT REFUSE']
 PREDICTIONS_COLORS = ['darkgreen', 'firebrick']
+SCORING_API_URI = "https://infinite-dawn-93145.herokuapp.com/"
+
 
 @st.cache
 def get_train_set():
@@ -46,19 +52,6 @@ def show_error(text):
         st.session_state.client_id = ''
 
 
-def cast_client_id():
-    """ Trys to convert the selected client ID into and int, and show an error if impossible
-
-    :return:
-    """
-    # Converting the selected client ID string into an int
-    try:
-        if st.session_state.client_id:
-            st.session_state.client_id = int(st.session_state.client_id)
-    except:
-        show_error("La valeur rentrée n'est pas un nombre")
-
-
 def show_sidebar(session_state):
     """ Display the dashboard sidebar """
     with st.sidebar:
@@ -70,6 +63,7 @@ def show_sidebar(session_state):
 
 
 def show_browsing_content():
+    """ Display the browsing content page """
     data = get_train_set()
 
     col_left, col_right = st.columns([2, 3])
@@ -92,34 +86,35 @@ def show_browsing_content():
 
         if selected_feature != '':
             # We use a sample to improve performances
-            data = data.sample(frac=0.2)
-
-            range = st.slider(
+            feature_range = st.slider(
                 'Choisissez un intervalle de valeurs',
                 min_value=float(data[selected_feature].min()),
                 max_value=float(data[selected_feature].max()),
                 value=[float(data[selected_feature].min()), float(data[selected_feature].max())]
             )
+            min_selected = feature_range[0]
+            max_selected = feature_range[1]
+
             separate_targets = st.checkbox("Séparer les clients acceptés et refusés")
 
             if separate_targets:
                 fig, axes = plt.subplots(figsize=(15, 5), ncols=2)
                 sns.histplot(
                     data=data[
-                        (data[selected_feature] >= range[0])
-                        & (data[selected_feature] <= range[1])
+                        (data[selected_feature] >= min_selected)
+                        & (data[selected_feature] <= max_selected)
                         & (data['TARGET'] == 0)
-                    ][selected_feature],
+                        ][selected_feature],
                     color=PREDICTIONS_COLORS[0],
                     ax=axes[0]
                 )
                 axes[0].set_title("Crédits acceptés", fontsize=15)
                 sns.histplot(
                     data=data[
-                        (data[selected_feature] >= range[0])
-                        & (data[selected_feature] <= range[1])
+                        (data[selected_feature] >= min_selected)
+                        & (data[selected_feature] <= max_selected)
                         & (data['TARGET'] == 1)
-                    ][selected_feature],
+                        ][selected_feature],
                     color=PREDICTIONS_COLORS[1],
                     ax=axes[1]
                 )
@@ -128,14 +123,15 @@ def show_browsing_content():
                 fig = plt.figure(figsize=(15, 7))
                 sns.histplot(
                     data=data[
-                        (data[selected_feature] >= range[0])
-                        & (data[selected_feature] <= range[1])
-                    ][selected_feature]
+                        (data[selected_feature] >= min_selected)
+                        & (data[selected_feature] <= max_selected)
+                        ][selected_feature]
                 )
             st.pyplot(fig)
 
 
 def show_scoring_content():
+    """ Display the scoring page content """
     col_left, col_right = st.columns(2)
 
     # DataFrames are set in cache using dedicated functions
@@ -155,6 +151,9 @@ def show_scoring_content():
                 # We display the prediction result with the probability
                 plot_score(predictions['prediction'], predictions['probability'])
 
+                # We display the local feature importance
+                plot_local_feature_importance(search_id)
+
                 # Then we add a multi-select to choose which features to see
                 features_to_show = st.multiselect(
                     'Sur quelles données souhaitez-vous situer le client ?',
@@ -165,7 +164,6 @@ def show_scoring_content():
                     plot_client_position(
                         train_set=train_set,
                         client_row=test_set[test_set['SK_ID_CURR'] == search_id],
-                        prediction=predictions['prediction'],
                         feature_name=feature_name
                     )
             else:
@@ -203,6 +201,7 @@ def show_scoring_content():
 
 
 def show_model_content():
+    """ Display the features importances page """
     col1, col2, col3 = st.columns([1, 3, 1])
 
     with col2:
@@ -222,17 +221,30 @@ def show_model_content():
 
 
 def get_model_features_importance():
-    response = requests.get('http://127.0.0.1:5000/features')
+    """ Calls the API to get the model global features importances """
+    response = requests.get(SCORING_API_URI + '/features')
     features = response.json()
     return features
 
+
 def predict_client(client_id):
-    response = requests.get('http://127.0.0.1:5000/' + str(client_id))
+    """ Calls the API to get the prediction and probability of the client
+
+    Args:
+        :param client_id: (int) the ID of the client to predict
+    """
+    response = requests.get(SCORING_API_URI + str(client_id))
     predictions = response.json()
     return predictions
 
 
 def plot_score(prediction, probability):
+    """ Plot the global score bar of the client
+
+    Args:
+        :param prediction: (int) whether the client is risky (1) or safe (0)
+        :param probability: (float) the probability of the prediction. Should be greater than 50%
+    """
     left_bar_w = probability if prediction == 1 else 1 - probability
     right_bar_w = probability if prediction == 0 else 1 - probability
 
@@ -278,14 +290,12 @@ def plot_score(prediction, probability):
     st.pyplot(fig)
 
 
-
-def plot_client_position(train_set, client_row, prediction, feature_name):
+def plot_client_position(train_set, client_row, feature_name):
     """ Plot the position of current customer into the feature distributions for each target
 
     Args:
         :param train_set: (pd.DataFrame) the train dataset features
         :param client_row: (pd.Series) the client ID
-        :param prediction: (int) the client prediction
         :param feature_name: (string) the name of the feature to plot inside X
     """
     fig, axes = plt.subplots(figsize=(20, 8), ncols=2)
@@ -335,3 +345,35 @@ def plot_client_position(train_set, client_row, prediction, feature_name):
     )
 
     st.pyplot(fig)
+
+
+def plot_local_feature_importance(client_id):
+    """ Plot the local feature importance figure
+
+    Args:
+        :param client_id: (int) the ID of the client in the test dataset
+    """
+    train_set = get_train_set()
+    test_set = get_test_set()
+    train_features = [x for x in train_set.columns if x not in [
+        'level_0', 'TARGET', 'SK_ID_CURR', 'isof_score'
+    ]]
+    test_features = [x for x in test_set.columns if x not in [
+        'SK_ID_CURR'
+    ]]
+    model = pickle.load(open(MODEL_PATH, 'rb'))
+
+    explainer = lime_tabular.LimeTabularExplainer(
+        train_set[train_features].to_numpy(),
+        mode="classification",
+        class_names=PREDICTIONS_LABELS,
+        feature_names=train_features,
+    )
+
+    explanation = explainer.explain_instance(
+        test_set[test_set['SK_ID_CURR'] == client_id][test_features].iloc[0],
+        model.predict_proba,
+        num_features=20
+    )
+
+    st.pyplot(explanation.as_pyplot_figure())
